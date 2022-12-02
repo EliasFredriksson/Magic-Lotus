984e6abb7c0470ccc6a08add769568f2fea15804
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-12-02 10:54:47 +0100

    Added certificates and HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { readFileSync } from "fs";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    https: {
+      key: readFileSync("./.cert/key.pem"),
+      cert: readFileSync("./.cert/cert.pem"),
+    }, // USE HTTPS PROTOCOL (YOU NEED TO PROVIDE YOUR OWN CERTIFICATE)
+    //cors: true, // USE CORS (default: true)
+    open: true, // OPEN NEW TAB WHEN SERVER STARTS
+    // headers: {},
+  },
+});
