2b7382fa3281fbda9c774139858cfe16bcfe6a7b
ASleepingBazza <elias.e.fredriksson@gmail.com>
2022-12-13 19:30:53 +0100

    Added font families. Added css default for main.scss. Added scss variables.

--- frontend/Magic-Lotus/src/Layout.tsx
+++ frontend/Magic-Lotus/src/Layout.tsx
@@ -1,11 +1,16 @@
-import "./layout.scss";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
 const Layout = () => {
   return (
-    <main className="layout">
+    <main
+      className="layout"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <Navbar />
       <div className="page-content">
         <Outlet />
